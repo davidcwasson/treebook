44efafb7cdf48857ecb04c841a4177e8aa593951
David Wasson <davidcwasson@gmail.com>
2013-12-31 11:18:47 -0600

    Forgot to save migration before commit last time, had to make a new migration and run it

--- db/migrate/20131231171724_add_user_id_to_statuses2.rb
+++ db/migrate/20131231171724_add_user_id_to_statuses2.rb
@@ -1,4 +1,4 @@
-class AddUserIdToStatuses < ActiveRecord::Migration
+class AddUserIdToStatuses2 < ActiveRecord::Migration
   def change
   	add_column :statuses, :user_id, :integer
   	add_index :statuses, :user_id
